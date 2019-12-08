3b48b5325dd25e3242c781730edcb445fdbfecf9
amit1103india <amit1103india@gmail.com>
2019-12-08 12:47:45 +0530

    Files Added

---
+++ anagrams.java
@@ -0,0 +1,38 @@
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public ArrayList<ArrayList<Integer>> anagrams(final List<String> A) {
+        LinkedHashMap<String, ArrayList<Integer>> globalMap = new LinkedHashMap<>();
+        
+        for(int i = 0; i < A.size(); i++){
+            String currStr = sortedString(A.get(i));
+            
+            //new entry
+            if(!globalMap.containsKey(currStr)){
+                ArrayList<Integer> data = new ArrayList<>();
+                data.add(i+1);
+                globalMap.put(currStr, data);
+            } else { //already existing
+                ArrayList<Integer> data = globalMap.get(currStr);
+                data.add(i+1);
+            }
+        }
+        
+        //collect result
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        
+        for(String key : globalMap.keySet()){
+            ArrayList<Integer> groupData = globalMap.get(key);
+            Collections.sort(groupData);
+            result.add(groupData);
+        }
+        
+        return result;
+        
+    }
+    
+    private String sortedString(String s){
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+}
---
+++ redundant brace.java
@@ -0,0 +1,50 @@
+public class Solution {
+    public int braces(String A) {
+        char[] paranthesis = new char[A.length()/2];
+        boolean[] consecutive = new boolean[A.length()/2];
+        
+        //initialization
+        int top = 0;
+        char prevChar = A.charAt(top);
+        
+        if(prevChar == '('){
+            paranthesis[top] = prevChar;
+            consecutive[top] = false;
+        }
+        
+        boolean operatorFound = false;
+        for(int i = 1; i < A.length(); i++){
+            char currChar = A.charAt(i);
+            
+            if(currChar == '('){
+                operatorFound = false;
+                top++;
+                paranthesis[top] = currChar;
+                if(prevChar == '('){
+                    consecutive[top] = true;
+                } else {
+                    consecutive[top] = false;
+                }
+            } else if(currChar == ')'){
+                //redundant check
+                if(i+1 < A.length() && A.charAt(i+1) == ')' && consecutive[top]){
+                    return 1;
+                }
+                
+                if(!operatorFound)
+                    return 1;
+                
+                //if not redundant, pop on brace
+                top--;
+            } else if (currChar == '+' || currChar == '-' 
+                        || currChar == '*' || currChar == '/'){
+                operatorFound = true;
+            }
+            
+            prevChar = currChar;
+        }
+        
+        return 0;
+        
+    }
+}
